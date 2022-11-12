50a2ff631809c35fc0df121aa086d49bcbe806bb
vaidikstackapp <113659859+vaidikstackapp@users.noreply.github.com>
2022-11-12 14:37:43 +0530

    xc
    rgb

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class MyApp extends StatelessWidget {
             fontFamily: 'Schyler1',
           ),
         )
+
         // MaterialApp(
         //   home: const SplashScreenView(),
         //   debugShowCheckedModeBanner: false,
